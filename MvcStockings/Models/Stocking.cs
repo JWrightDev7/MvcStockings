080f0aaef188cfc158788e5d0d7772655f4bb56d
JWrightDev7 <james.wright01@stclairconnect.ca>
2021-10-13 18:20:11 -0400

    Updated validation, some styling, added some content to pages

--- MvcStockings/Models/Stocking.cs
+++ MvcStockings/Models/Stocking.cs
@@ -10,15 +10,16 @@ namespace MvcStockings.Models
     public class Stocking
     {
         public int Id { get; set; }
-        [StringLength(25, MinimumLength =3)] //Sets the max and min length of the string
+        [StringLength(60, MinimumLength =3)] //Sets the max and min length of the string
         [Required] //Makes the field Required
         public string Name { get; set; }
-        [StringLength(25, MinimumLength =3)] //Sets the max and min length of the string
+        [StringLength(50, MinimumLength =3)] //Sets the max and min length of the string
         [Required] //Makes the field Required
         public string Type { get; set; }
-        [StringLength(25, MinimumLength =3)] //Sets the max and min length of the string
+        [StringLength(50, MinimumLength =3)] //Sets the max and min length of the string
         [Required] //Makes the field Required
         public string Material { get; set; }
+        [DataType(DataType.Currency)]//Sets Currency as the formatting
         [Column(TypeName ="double(18,2")] //Sets the formatting for the number to 2 digits after the decimal
         public double Price { get; set; }
         [Range(1, 5)] //Sets a max value of 5 and minimum value of 1
